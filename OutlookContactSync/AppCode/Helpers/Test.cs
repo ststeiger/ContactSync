1abccd8bd5c555ab321dfba6cfee49cfa39d1009
ststeiger <quandary82@hailmail.net>
2015-02-20 17:44:10 +0100

    Test class

---
+++ OutlookContactSync/AppCode/Helpers/Test.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace OutlookContactSync.AppCode.Helpers
+{
+
+
+    class Test
+    {
+
+
+        public void foo()
+        {
+
+        }
+
+
+    }
+
+
+}
